9425ebc2b34fac36f284d33b49e093043570f49b
Zara <zaratam12@gmail.com>
2020-01-13 21:25:42 +0000

    WIP

--- problem_set_2/caesar.c
+++ problem_set_2/caesar.c
@@ -8,20 +8,23 @@ int main(int argc, string argv[])
     if (argc == 2)
     {
         string k = argv[1];
-        for (int i = 0, n = strlen(argv[1]); i < n; i++)
-        {
-            if (isdigit(argv[1][i]))
-            {
-                return 0;
-            }
-            else
-            {
-                return 1;
-            }
-        }
+//         for (int i = 0, n = strlen(argv[1]); i < n; i++)
+//         {
+//             if (isdigit(argv[1][i]))
+//             {
+//                 return 0;
+//             }
+//             else
+//             {
+//                 return 1;
+//             }
+//         }
         
+        int key = atoi(k);
         printf("%s\n", k);
+        printf("%i\n", key);
         string plaintext = get_string("plaintext: ");
+        printf("%c\n", plaintext[0]);
 
         string ciphertext = "";
         for (int i = 0, n = strlen(plaintext); i < n; i++)
@@ -37,7 +40,6 @@ int main(int argc, string argv[])
             {
                 ciphertext[i] = plaintext[i];
             }
-            
         }
         printf("ciphertext: %s\n", ciphertext);
         return 0;
