9301f20cfcb51de1433b3652cf0c45a6f43f69fb
Zara <zaratam12@gmail.com>
2020-01-13 22:33:48 +0000

    Refactor WIP

--- problem_set_2/caesar.c
+++ problem_set_2/caesar.c
@@ -3,56 +3,58 @@
 #include <string.h>
 #include <cs50.h>
 
+int check_argument(string k);
+
 int main(int argc, string argv[])
 {
     if (argc == 2)
     {
         string k = argv[1];
-//         for (int i = 0, n = strlen(argv[1]); i < n; i++)
-//         {
-//             if (isdigit(argv[1][i]))
-//             {
-//                 return 0;
-//             }
-//             else
-//             {
-//                 return 1;
-//             }
-//         }
-        
-        int key = atoi(k);
+        check_argument(k);
+
+        int key = atoi(k) % 26;
         printf("k = %s\n", k);
         printf("key = %i\n", key);
-        printf("key mod 26 = %i\n", key % 26);
         string plaintext = get_string("plaintext: ");
-        printf("plaintext = %c\n", plaintext[0]);
-
-
         string ciphertext = "";
         for (int i = 0, n = strlen(plaintext); i < n; i++)
         {
             // If character is from A to Z, or from a to z
             if ((plaintext[i] >= 65 && plaintext[i] <= 90) || (plaintext[i] >= 97 && plaintext[i] <= 122))
             {
-                ciphertext[i] = plaintext[i] + key % 26;
+                ciphertext[i] = plaintext[i] + key;
                 // TODO handle wrapping around alphabetically, e.g. Z as A
             }
             else
             {
                 ciphertext[i] = plaintext[i];
             }
+            printf("ciphertext: %s\n", ciphertext);
+            return 0;
         }
-        printf("ciphertext: %s\n", ciphertext);
-        return 0;
-    }
-    else if (argc == 1)
-    {
-        printf("Empty input\n");
-        return 1;
     }
     else
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
+}
+
+int check_argument(string k)
+{
+    for (int i = 0, n = strlen(k); i < n; i++)
+        {
+            if (isdigit(k[i]) != 0)
+            {
+                printf("Digit\n");
+            }
+            else
+            {
+                printf("Not digit\n");
+                return 1;
+            }
+        }
+    // TODO turn string to int only if argument is all digits
+    int key = atoi(k) % 26;
+    return key;
 }
