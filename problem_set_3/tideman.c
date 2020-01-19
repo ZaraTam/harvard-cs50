db946263fb88ccb0828c96246f3d92160a9db80e
Zara <zaratam12@gmail.com>
2020-01-19 16:20:03 +0000

    Update add_pairs function

--- problem_set_3/tideman.c
+++ problem_set_3/tideman.c
@@ -128,7 +128,32 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            if (preferences[i][j] > preferences[j][i])
+            {
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                pair_count++;
+
+                printf("Winner = %i\n", i);
+                printf("Loser = %i\n", j);
+                printf("Pair count = %i\n", pair_count);
+            }
+            else if (preferences[i][j] < preferences[i][j])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                pair_count++;
+
+                printf("Winner = %i\n", i);
+                printf("Loser = %i\n", j);
+                printf("Pair count = %i\n", pair_count);
+            }
+        }
+    }
     return;
 }
 
