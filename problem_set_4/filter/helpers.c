3db3867d4f83bd684980fb5ec0de58862c47f832
Zara <zaratam12@gmail.com>
2020-01-26 22:20:32 +0000

    Update styling

--- problem_set_4/filter/helpers.c
+++ problem_set_4/filter/helpers.c
@@ -9,7 +9,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
+            average = round((image[i][j].rgbtBlue
+                            + image[i][j].rgbtGreen
+                            + image[i][j].rgbtRed) / 3);
+
             image[i][j].rgbtBlue = average;
             image[i][j].rgbtGreen = average;
             image[i][j].rgbtRed = average;
