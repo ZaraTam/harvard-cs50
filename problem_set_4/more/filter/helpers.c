f6f97961fb42367e2430f7ff682b83f9e0dbcaad
Zara <zaratam12@gmail.com>
2020-01-26 22:54:12 +0000

    Update blur function

--- problem_set_4/more/filter/helpers.c
+++ problem_set_4/more/filter/helpers.c
@@ -50,6 +50,57 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE temp_image[height][width];
+    float sum_blue, sum_green, sum_red;
+    float average_blue, average_green, average_red;
+    int average_count;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            temp_image[i][j].rgbtBlue = image[i][j].rgbtBlue;
+            temp_image[i][j].rgbtGreen = image[i][j].rgbtGreen;
+            temp_image[i][j].rgbtRed = image[i][j].rgbtRed;
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            sum_blue = 0;
+            sum_green = 0;
+            sum_red = 0;
+            average_count = 0;
+
+            for (int x = i - 1; x <= i + 1; x++)
+            {
+                for (int y = j - 1; y <= j + 1; y++)
+                {
+                    if (x < 0 || x >= height || y < 0 || y >= width)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        sum_blue += temp_image[x][y].rgbtBlue;
+                        sum_green += temp_image[x][y].rgbtGreen;
+                        sum_red += temp_image[x][y].rgbtRed;
+                        average_count++;
+                    }
+                }
+            }
+
+            average_blue = round(sum_blue / average_count);
+            average_green = round(sum_green / average_count);
+            average_red = round(sum_red / average_count);
+
+            image[i][j].rgbtBlue = average_blue;
+            image[i][j].rgbtGreen = average_green;
+            image[i][j].rgbtRed = average_red;
+        }
+    }
     return;
 }
 
