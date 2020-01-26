777d253e6665e0e5de475ebb07787fce6d31ad9c
Zara <zaratam12@gmail.com>
2020-01-26 22:53:33 +0000

    Update reflect function

--- problem_set_4/more/filter/helpers.c
+++ problem_set_4/more/filter/helpers.c
@@ -24,6 +24,26 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE temp_image[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            temp_image[i][j].rgbtBlue = image[i][j].rgbtBlue;
+            temp_image[i][j].rgbtGreen = image[i][j].rgbtGreen;
+            temp_image[i][j].rgbtRed = image[i][j].rgbtRed;
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtBlue = temp_image[i][width - 1 - j].rgbtBlue;
+            image[i][j].rgbtGreen = temp_image[i][width - 1 - j].rgbtGreen;
+            image[i][j].rgbtRed = temp_image[i][width - 1 - j].rgbtRed;
+        }
+    }
     return;
 }
 
