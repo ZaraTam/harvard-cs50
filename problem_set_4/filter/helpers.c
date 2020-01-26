1452d2e4f551bdc95adafac49dbc00288a544462
Zara <zaratam12@gmail.com>
2020-01-26 22:17:58 +0000

    Update styling

--- problem_set_4/filter/helpers.c
+++ problem_set_4/filter/helpers.c
@@ -26,9 +26,18 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            sepiaBlue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
-            sepiaGreen = round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
-            sepiaRed = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
+            sepiaBlue = round(0.272 * image[i][j].rgbtRed
+                            + 0.534 * image[i][j].rgbtGreen
+                            + 0.131 * image[i][j].rgbtBlue);
+
+            sepiaGreen = round(0.349 * image[i][j].rgbtRed
+                            + 0.686 * image[i][j].rgbtGreen
+                            + 0.168 * image[i][j].rgbtBlue);
+
+            sepiaRed = round(0.393 * image[i][j].rgbtRed
+                            + 0.769 * image[i][j].rgbtGreen
+                            + 0.189 * image[i][j].rgbtBlue);
+
             image[i][j].rgbtBlue = (sepiaBlue < 255) ? sepiaBlue : 255;
             image[i][j].rgbtGreen = (sepiaGreen < 255) ? sepiaGreen : 255;
             image[i][j].rgbtRed = (sepiaRed < 255) ? sepiaRed : 255;
