80a6c65d0253370b56fc15e9edf3bd908e51a4ec
Zara <zaratam12@gmail.com>
2020-03-14 16:43:00 +0000

    WIP check if JPEG header

--- problem_set_4/recover/recover/recover.c
+++ problem_set_4/recover/recover/recover.c
@@ -16,13 +16,20 @@ int main(int argc, char *argv[])
     }
 
     // Check JPEG signature in each block of 512 bytes
-    unsigned char buffer[512];
-    fread(buffer, 1, 512, file);
+    int block_size = 512;
+    unsigned char buffer[block_size];
+    int byte_count = fread(buffer, 1, block_size, file);
+
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
     {
         if ((buffer[3] & 0xf0) == 0xe0)
         {
             printf("jpg\n");
         }
+        
+    }
+    else
+    {
+        printf("Not jpg\n");
     }
 }
