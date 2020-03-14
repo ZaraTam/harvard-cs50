37a315191538fd73fad671563b91d330de2d7ef6
Zara <zaratam12@gmail.com>
2020-03-14 15:22:34 +0000

    Open file

--- problem_set_4/recover/recover/recover.c
+++ problem_set_4/recover/recover/recover.c
@@ -3,5 +3,15 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        printf("Usage: ./recover image");
+        return 1;
+    }
 
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        return 1;
+    }
 }
