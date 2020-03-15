c296263ea498fa7eaef58eaa4832a5e188330a6d
Zara <zaratam12@gmail.com>
2020-03-15 19:56:54 +0000

    Update index and grades

--- problem_set_6/readability.py
+++ problem_set_6/readability.py
@@ -2,13 +2,6 @@ from cs50 import get_string
 
 text = get_string("Text: ")
 
-# Coleman-Liau index
-# L = average number of letters per 100 words in the text
-# S = average number of sentences per 100 words in the text
-L = 0
-S = 0
-index = 0.0588 * L - 0.296 * S - 15.8
-
 
 def count_letters(text):
 # Letters are any uppercase or lowercase alphabetic characters, 
@@ -55,8 +48,17 @@ letter_count = count_letters(text)
 word_count = count_words(text)
 sentence_count = count_sentences(text)
 
-print("letter_count =", letter_count)
-print("word_count =", word_count)
-print("sentence_count =", sentence_count)
+# Coleman-Liau index
+# L = average number of letters per 100 words in the text
+# S = average number of sentences per 100 words in the text
+L = letter_count / word_count * 100
+S = sentence_count / word_count * 100
+index = 0.0588 * L - 0.296 * S - 15.8
+grade = round(index)
 
-print()
+if grade < 1:
+    print("Before Grade 1")
+elif grade >= 16:
+    print("Grade 16+")
+else:
+    print("Grade", grade)
