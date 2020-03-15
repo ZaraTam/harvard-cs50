80f572fad7670323925c9c18c0c3e58ab6ca5d76
Zara <zaratam12@gmail.com>
2020-03-15 10:28:59 +0000

    Add mario_less

---
+++ problem_set_6/mario_less.py
@@ -0,0 +1,10 @@
+from cs50 import get_int
+
+while True:
+    height = get_int("Height: ")
+    if 1 <= height <= 8:
+        break
+
+for i in range(height):
+    print(" " * (height - i - 1), end="")
+    print("#" * (i + 1))
