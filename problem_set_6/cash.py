052539098ebc8f999c12577019032b5d525290d8
Zara <zaratam12@gmail.com>
2020-03-15 12:01:40 +0000

    Add cash

---
+++ problem_set_6/cash.py
@@ -0,0 +1,24 @@
+from cs50 import get_float
+
+while True:
+    change = get_float("Change owed: ") * 100
+    if change >= 0:
+        break
+
+coin_count = 0
+
+while change > 0:
+    while change >= 25:
+        coin_count += change // 25
+        change = change % 25
+    while change >= 10:
+        coin_count += change // 10
+        change = change % 10
+    while change >= 5:
+        coin_count += change // 5
+        change = change % 5
+    while change >= 1:
+        coin_count += change // 1
+        change = change % 1
+
+print(int(coin_count))
