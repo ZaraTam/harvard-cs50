305798cd1d6efb4274951a7d44dc91302bba14fc
Zara <zaratam12@gmail.com>
2020-03-21 15:28:11 +0000

    WIP read str file header

--- problem_set_6/dna.py
+++ problem_set_6/dna.py
@@ -2,15 +2,25 @@ import csv
 from sys import argv, exit
 
 if len(argv) != 3:
-    print("Missing command line arguments")
+    print("Usage: python dna.py data.csv sequence.txt")
     exit(1)
 
 str_filename = argv[1]
 dna_filename = argv[2]
 
 with open(str_filename, "r") as str_file:
-    str_read = str_file.read()
-    print(str_read)
+    str_read = csv.DictReader(str_file)
+
+    for row in str_read:
+        print(row)
+        name = row["name"]
+        print(name)
+
+    str = []
+    for key in row:
+        str.append(key)
+    str.pop(0)
+    print(str)
 
 with open(dna_filename, "r") as dna_file:
     dna_read = dna_file.read()
