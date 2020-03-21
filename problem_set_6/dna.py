75fc40d4299f9f28b9e188009e3d076c256fdb5c
Zara <zaratam12@gmail.com>
2020-03-21 16:37:48 +0000

    WIP count STR in DNA

--- problem_set_6/dna.py
+++ problem_set_6/dna.py
@@ -16,12 +16,41 @@ with open(str_filename, "r") as str_file:
         name = row["name"]
         print(name)
 
-    str = []
+    str_data = []
     for key in row:
-        str.append(key)
-    str.pop(0)
-    print(str)
+        str_data.append(key)
+    str_data.pop(0)
+    print(str_data)
 
 with open(dna_filename, "r") as dna_file:
     dna_read = dna_file.read()
     print(dna_read)
+
+    dna_str_count = {}
+
+    for str in str_data:
+        str_count = 0
+        str_max_count = 0
+        i = 0
+        str_length = len(str)
+
+        while i < len(dna_read) - str_length:
+            if dna_read[i:i+str_length] == str:
+
+                print(str)
+                print(dna_read[i:i+str_length])
+                str_count += 1
+                if str_count > str_max_count:
+                    str_max_count = str_count
+                i += str_length
+
+                print("str_max_count =", str_max_count)
+                print("str_count", str_count)
+
+            else:
+                str_count = 0
+                i += 1
+
+        dna_str_count[str] = str_max_count
+
+    print(dna_str_count)
