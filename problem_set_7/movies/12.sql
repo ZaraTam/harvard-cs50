89cc4a36c7cc0961aedabd60982b8545f712f9be
Zara <zaratam12@gmail.com>
2020-03-22 18:50:46 +0000

    Shorter runtime

--- problem_set_7/movies/12.sql
+++ problem_set_7/movies/12.sql
@@ -1,11 +1,6 @@
 SELECT title FROM
 movies JOIN stars ON movies.id = stars.movie_id JOIN
 people ON stars.person_id = people.id
-WHERE name = "Johnny Depp"
-
-INTERSECT
-
-SELECT title FROM
-movies JOIN stars ON movies.id = stars.movie_id JOIN
-people ON stars.person_id = people.id
-WHERE name = "Helena Bonham Carter";
+WHERE name = "Johnny Depp" OR name = "Helena Bonham Carter"
+GROUP BY title
+HAVING COUNT(title) = 2;
