34cabccf26e0c91e3e8d6131a53463df8e976679
Zara <zaratam12@gmail.com>
2020-03-22 23:37:50 +0000

    Enforce uniqueness

--- problem_set_7/movies/12.sql
+++ problem_set_7/movies/12.sql
@@ -2,5 +2,5 @@ SELECT title FROM
 movies JOIN stars ON movies.id = stars.movie_id JOIN
 people ON stars.person_id = people.id
 WHERE name = "Johnny Depp" OR name = "Helena Bonham Carter"
-GROUP BY movies.id
-HAVING COUNT(movies.id) = 2;
+GROUP BY title, movies.id
+HAVING COUNT(title) = 2;
