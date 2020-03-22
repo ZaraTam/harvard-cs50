423bc75d28c8d9d3a2595b3889f1bd6b401db9ef
Zara <zaratam12@gmail.com>
2020-03-22 16:49:17 +0000

    Update SQL query

--- problem_set_7/movies/9.sql
+++ problem_set_7/movies/9.sql
@@ -1,6 +1,5 @@
-SELECT name FROM
-people JOIN stars ON people.id = stars.person_id JOIN
-movies ON stars.movie_id = movies.id
-WHERE year = 2004
-GROUP BY name
+SELECT name FROM people
+WHERE id IN (SELECT DISTINCT person_id FROM
+stars JOIN movies ON stars.movie_id = movies.id
+WHERE movies.year = 2004)
 ORDER BY birth;
