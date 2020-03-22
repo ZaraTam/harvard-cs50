6df1a7970f877451078d8bc5b81ba3d49e1116d7
Zara <zaratam12@gmail.com>
2020-03-22 16:59:12 +0000

    Update SQL query

--- problem_set_7/movies/7.sql
+++ problem_set_7/movies/7.sql
@@ -1,5 +1,4 @@
 SELECT title, rating FROM
 movies JOIN ratings ON movies.id = ratings.movie_id
 WHERE year = 2010 AND rating IS NOT NULL
-GROUP BY title
-ORDER BY rating DESC;
+ORDER BY rating DESC, title;
