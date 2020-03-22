c8ba9e2faaff59127db947f61792319ee70f17df
Zara <zaratam12@gmail.com>
2020-03-22 17:00:45 +0000

    Update SQL query

--- problem_set_7/movies/10.sql
+++ problem_set_7/movies/10.sql
@@ -1,4 +1,4 @@
-SELECT name FROM
+SELECT DISTINCT name FROM
 people JOIN directors ON people.id = directors.person_id JOIN
 movies ON directors.movie_id = movies.id JOIN
 ratings ON movies.id = ratings.movie_id
