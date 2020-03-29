dfc9cfb2209e3a00cc946771f3a59b0a432c84e5
Zara <zaratam12@gmail.com>
2020-03-29 18:49:35 +0100

    WIP check card type

--- problem_set_1/credit.c
+++ problem_set_1/credit.c
@@ -65,21 +65,41 @@ bool check_length(long number, int length)
 
 string check_card_type(long number, int length)
 {
-    string card_type;
+    string card_type = "";
     int numbers[length];
-
-    // All American Express numbers start with 34 or 37
-    // Most MasterCard numbers start with 51, 52, 53, 54, or 55
-    // All Visa numbers start with 4
 
     for (int i = 0; i < length; i++)
     {
         numbers[i] = number % 10;
-        printf("Number %lu\n", number);
-        printf("Digit %i\n", numbers[i]);
+        printf("Number = %ld\n", number);
+        printf("index = %i\n", i);
+        printf("Digit = %i\n", numbers[i]);
+        printf("\n");
         number /= 10;
     }
 
-    card_type = "Invalid";
+    // All Visa numbers start with 4
+    if (length >= 1 && numbers[length - 1] == 4)
+    {
+        card_type = "VISA";
+    }
+
+    // All American Express numbers start with 34 or 37
+    if (length >= 2 && numbers[length - 1] == 3
+        && (numbers[length - 2] == 4 || numbers[length - 2] == 7))
+    {
+        card_type = "AMEX";
+    }
+
+    // Most MasterCard numbers start with 51, 52, 53, 54, or 55
+    if (length >= 2 && numbers[length - 1] == 5
+        && (numbers[length - 2] == 1 || numbers[length - 2] == 2
+            || numbers[length - 2] == 3 || numbers[length - 2] == 4
+            || numbers[length - 2] == 5))
+    {
+        card_type = "MASTERCARD";
+    }
+
+    printf("Card type = %s\n", card_type);
     return card_type;
 }
